4ccb8907a5e7da6535c8919706b70dcaf4556d63
Brandon Kugsang Jeong <brandon.kugsang.jeong@gmail.com>
2019-04-03 18:57:57 -0400

    initial work

---
+++ main.py
@@ -0,0 +1,13 @@
+
+from add import add
+from subtract import subtract
+from multiply import multiply
+from divide import divide
+
+print("GIT Working")
+a = 10
+b = 5
+print("ADD: ", add(a, b))
+print("SUB: ", add(a, b))
+print("MUL: ", add(a, b))
+print("DIV: ", add(a, b))
---
+++ subtract.py
@@ -0,0 +1,4 @@
+
+
+def subtract(a,b):
+    return 0
